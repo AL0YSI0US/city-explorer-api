9fe0443289fe0c013b4a93418ef387780ffab2af
aloysious x <alicia.todd.army@gmail.com>
2021-04-15 16:40:56 -0700

    Back-end API Task List: completed

--- server.js
+++ server.js
@@ -14,21 +14,30 @@ const PORT = process.env.PORT || 3001;
 // listening for GET requests at the path
 
 // take in in two arguments: the path and the response
-app.get('/weather', (resuest, response) => {
-  const allDailyForecasts = weather.data.map(day => new allDailyForecasts(day));
+app.get('/weather', (request, response) => {
+  const dailyForecasts = weatherData.data.map(day => new Forecasts(day));
   // when we get the request, send a response that says returns some forecasts
   // response has some methods that are very helpful such as teh send method
-  response.send(allDailyForecasts);
+  response.send(dailyForecasts);
 })
-
-function DailyForecast(day) {
+// "data munging"
+function Forecasts(day) {
   this.date = day.datetime;
-  this.description;
+  this.description = day.weather.description;
+  this.lowTemp = day.low_temp;
+  this.hiTemp = day.max_temp;
 }
 
-app.get('/weather', (request, response) => {
-  response.json(weatherData);
-}
+// TODO
+// come back later to ensure that I am requesting the lat and long
+// that was skipped early on in lab 7 [cannot be done locally]
+// can be imported from 8bit though:
+// https://api.weatherbit.io/v2.0/forecast/daily
+// [english] &lang=en
+// [degrees in F] &units=I
+// [optional / callback function] &callback=${}
+// [populate a week] &days=7 
+// [lattitude and longitude] &lat&lon&
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
 
